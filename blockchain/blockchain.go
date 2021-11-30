f976b1accb4006c6f7f5c19fd02dbc0ed346da8d
serhatYilmazz <yilmazserhat030@gmail.com>
2021-11-30 21:29:22 +0300

    Block and Blockchain is created at first place. Basic structuring the blockchain.

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -6,24 +6,24 @@ const (
 	GENESIS = "Genesis"
 )
 
-type BlockChain struct {
+type Blockchain struct {
 	blocks []block.Block
 }
 
-func initBlockchain() *BlockChain {
-	return &BlockChain{
+func initBlockchain() *Blockchain {
+	return &Blockchain{
 		blocks: []block.Block{
 			*block.CreateBlock(GENESIS, nil),
 		},
 	}
 }
 
-func (bc *BlockChain) genesisBlock(data string) {
+func (bc *Blockchain) genesisBlock(data string) {
 	newBlock := block.CreateBlock(data, []byte(GENESIS))
 	bc.blocks = append(bc.blocks, *newBlock)
 }
 
-func (bc *BlockChain) AddBlock(data string) {
+func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := block.CreateBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, *newBlock)
