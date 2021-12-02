3204e8ca910710c15a06c4e79e80a1a304955f29
serhatYilmazz <yilmazserhat030@gmail.com>
2021-12-02 14:24:33 +0300

    BadgerDB is added to blockchain and genesis block is created in case of not existing first block.

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -2,31 +2,62 @@ package blockchain
 
 import (
 	"fmt"
+	"github.com/dgraph-io/badger"
 	"goblockchain/block"
 )
 
 const (
 	GENESIS = "Genesis"
+	dbPath  = "./tmp/blockchain"
+	LastHash = "lh"
 )
 
 type Blockchain struct {
-	Blocks []block.Block
+	lastHash []byte
+	Database *badger.DB
 }
 
-func initBlockchain() *Blockchain {
-	return &Blockchain{
-		Blocks: []block.Block{
-			*block.CreateBlock(GENESIS, nil),
-		},
+
+func InitBlockchain() *Blockchain {
+	db, err := badger.Open(badger.DefaultOptions(dbPath))
+	bc := &Blockchain{
+		lastHash: nil,
+		Database: db,
 	}
+	HandleError(err)
+	err = db.Update(func(txn *badger.Txn) error {
+		if lh, rerr := txn.Get([]byte(LastHash)); rerr == badger.ErrKeyNotFound {
+			fmt.Println("Blockchain does not exist.")
+			genesis := genesis()
+			serializedBlock := genesis.Serialize()
+			bc.lastHash = genesis.Hash
+			rerr := txn.Set(genesis.Hash, serializedBlock)
+			HandleError(rerr)
+
+			rerr = txn.Set([]byte(LastHash), genesis.Hash)
+			HandleError(rerr)
+			return nil
+		} else {
+			fmt.Println("Last hash is ", lh)
+			bc.lastHash, rerr = lh.ValueCopy([]byte{})
+			HandleError(rerr)
+			return nil
+		}
+	})
+	HandleError(err)
+
+	return bc
 }
 
-func (bc *Blockchain) AddBlock(data string) error {
-	if len(bc.Blocks) != 1 {
-		return fmt.Errorf("Initialize blockchain before add a new block")
+func genesis() *block.Block {
+	createdBlock := block.CreateBlock(GENESIS, nil)
+	return createdBlock
+}
+
+}
+
+func HandleError(err error) {
+	if err != nil {
+		fmt.Println(err)
 	}
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := block.CreateBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, *newBlock)
-	return nil
 }
