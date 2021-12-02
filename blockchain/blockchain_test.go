90453eee5a4c8c5cd502712015a6b4e4b56d1cb3
serhatYilmazz <yilmazserhat030@gmail.com>
2021-12-02 16:16:38 +0300

    Database is closed at the end of the test

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -8,21 +8,24 @@ import (
 
 func TestBlockChain_genesisBlock(t *testing.T) {
 	bc := InitBlockchain()
+	defer bc.Database.Close()
 	fmt.Println(len(bc.lastHash))
 	if len(bc.lastHash) != 64 {
 		t.Errorf("There should be exactly one block in blockchain")
 	}
+	cleanDb(bc)
 }
 
 func TestBlockChain_AddBlock(t *testing.T) {
 	s := "This is an example block data"
 	bc := InitBlockchain()
+	defer bc.Database.Close()
 	block := bc.AddBlock(s)
-	blockDb := bc.GetBlockByHash(bc.lastHash)
+	blockDb := GetBlockByHash(bc.Database, bc.lastHash)
 	if block.Nonce != blockDb.Nonce {
 		t.Errorf("AddBlock does not run properly")
 	}
-	deleteFromBlockchain(bc, block.Hash)
+	cleanDb(bc)
 }
 
 func deleteFromBlockchain(bc *Blockchain, hashToDelete []byte) {
